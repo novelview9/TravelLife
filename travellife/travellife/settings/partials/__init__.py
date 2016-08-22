91de3c7351a8ef94e819a33525dc36ac4b281669
jupiny <tmdghks584@gmail.com>
2016-08-23 07:13:19 +0900

    add celery beat settings

--- travellife/travellife/settings/partials/__init__.py
+++ travellife/travellife/settings/partials/__init__.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+
 import dj_database_url
+from celery.schedules import crontab
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,18 +122,28 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+TIME_ZONE = 'Asia/Seoul'
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Using Redis
+# http://docs.celeryproject.org/en/latest/getting-started/brokers/redis.html
+
+BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_ACCEPT_CONTENT = ['pickle', 'json']
+
+CELERYBEAT_SCHEDULE = {
+    'update-tour-model': {
+        'task': 'tour.tasks.update_tour_model.UpdateTourModelTask',
+        'schedule': crontab(
+            hour=21,
+            minute=0,
+        ),
+    },
+}
