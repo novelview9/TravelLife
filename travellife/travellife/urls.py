b2e7f2926cc6132391cebf9ebc988e86b4e27140
lioliolio <kpc00100@gmail.com>
2016-08-22 20:53:50 +0900

    fix urls.py

--- travellife/travellife/urls.py
+++ travellife/travellife/urls.py
@@ -1,16 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
-from . import views
 
 from tmap.views import *
 from tour.views import *
-from intro import views
-from django.conf import settings
+from intro.views import *
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^tmap/$', tmap),
     url(r'^tour/$', CreateAPIView.as_view()),
-    url(r'^', views.IntroView.as_view(), name="intro"),
+    url(r'^', IntroView.as_view(), name="intro"),
     url(r'^address/$', AddressToPointView.as_view(), name="address"),
 ]
